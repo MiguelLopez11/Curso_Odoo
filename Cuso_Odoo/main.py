14f5a2f3f30f1a5d99a9b1bff97cfcfc469e2096
Miguel López <miguellopezo1107@gmail.com>
2021-08-25 23:01:17 -0500

    Prueba

--- Cuso_Odoo/main.py
+++ Cuso_Odoo/main.py
@@ -1,7 +1,20 @@
-def Nombre():
-    print("¿Como te llamas?")
-    Nom = input()
-    print(f"¡Bienvenido {Nom}!")
+import Apellidos
 
 
-Nombre()
+class Nombre:
+    def __init__(self):
+        print("")
+    def Nombre(self):
+        print("¿Como te llamas?")
+        self.Nom = input()
+
+    def GetApellidos(self):
+        self.Apell = Apellidos.Apellidos()
+
+        print(f"¡Bienvenido {self.Nom, self.Apell}!")
+
+
+
+
+Nombre.Nombre()
+Nombre.GetApellidos()
